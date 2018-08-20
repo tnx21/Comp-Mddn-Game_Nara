dbabff072fa198287a95ffb39aa96abf8a2189c4
MehauWilliams <williams.xrd@gmail.com>
2018-08-20 21:43:50 +1200

    Visual update, more assets #3

--- Prototype_1/Assets/Scripts/Mehau's Scripts/Destructible.cs
+++ Prototype_1/Assets/Scripts/Mehau's Scripts/Destructible.cs
@@ -11,7 +11,7 @@ public class Destructible : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter(Collision collision){
-		if(collision.impactForceSum.magnitude > 0.1f){
+		if(collision.impactForceSum.magnitude > 10f){
 			DestroyMe();
 		}
 	}
