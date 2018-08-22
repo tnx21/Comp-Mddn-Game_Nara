6f07a263e956939579a38ddcd2d24c44d214a752
MehauWilliams <williams.xrd@gmail.com>
2018-08-23 04:12:58 +1200

    Tidied heirarchy and made game ready #1

--- Prototype_1/Assets/Scripts/Sean's Scripts/AdvancedMovement.cs
+++ Prototype_1/Assets/Scripts/Sean's Scripts/AdvancedMovement.cs
@@ -14,20 +14,29 @@ public class AdvancedMovement : MonoBehaviour
     int moveForce = 150;         // strength of push used for movement
 
     // jumping movement controls
-    int jumpForce = 1250;       // strength of push used for jumping
+    float jumpForce = 1000f;       // strength of push used for jumping
     bool jumping = false;       // has the object used jump
     bool doubleJumping = false; // has the object used double jump
 
     // dash movement controls
-    float lastA = -3;
-    float lastD = -3;
-    bool dash = false;
+    float lastA = -3;   // used for timing double tap in dash
+    float lastD = -3;   // used for timing double tap in dash
+    bool dashing = false;  // is object dashing
+    float dashTimer = -3; // when dash was used last
+    float dashMulti = 2.5f; // multiplier used to multiply jumpForce in the dash movements
 
     // Checks if the player touches a platform and resets jumping capability if they do
-    private void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision)
     {
+        if (dashing)
+        {
+            if (CheckTime(dashTimer) > 1) {
+
+            }
+        }
         if (collision.gameObject.layer == groundLayer)
         {
+            // resets ability to jump/ double jump
             jumping = false;
             doubleJumping = false;
         }
@@ -41,32 +50,27 @@ public class AdvancedMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+
         // jumping movement
         if (Input.GetKeyDown(KeyCode.Space) && (!jumping || !doubleJumping))
-        {
-            //rigbody.velocity = 0;
-            rigbody.AddForce(0, jumpForce, 0);
+        {            
             if (!jumping)
             {
+                rigbody.AddForce(0, jumpForce, 0);
                 jumping = true;
             }
             else
             {
+                rigbody.AddForce(0, jumpForce * 1.25f, 0);
                 doubleJumping = true;
             }
-        }
-        // forward movement
-        if (Input.GetKey("w") && rigbody.velocity[2] < maxVelocity)
-        {
-            rigbody.AddForce(0, 0, moveForce);
-            Debug.Log(rigbody.velocity);
         }
         // ground pound movement
         if (Input.GetKey("s") && (jumping || doubleJumping))
         {
-            rigbody.velocity.Set(0, 0, 0);
-            rigbody.AddForce(0, -jumpForce/2, 0);
+            rigbody.velocity.Set(0, 0, 0);      // stop directional movement
+            rigbody.AddForce(0, -jumpForce/2, 0);   // apply downwards force
         }
         // right movement
         if (Input.GetKey("d") && rigbody.velocity[0] < maxVelocity)
@@ -75,10 +79,12 @@ public class AdvancedMovement : MonoBehaviour
         }
         // right dash
         if (Input.GetKeyDown("d"))
-        {
-            if (CheckTime(lastD) < 0.5)
+        {   
+            if (CheckTime(lastD) < 0.25 && CheckTime(dashTimer) > 3)
             {
-                rigbody.AddForce(jumpForce * 5, 0, 0);
+                rigbody.AddForce(jumpForce*dashMulti, 0, 0);
+                dashTimer = Time.time;
+                dashing = true;
             }
             else
             {
@@ -90,11 +96,14 @@ public class AdvancedMovement : MonoBehaviour
         {
             rigbody.AddForce(-moveForce, 0, 0);
         }
+        // left dash
         if (Input.GetKeyDown("a"))
         {
-            if (CheckTime(lastA) < 0.5)
+            if (CheckTime(lastA) < 0.25 && CheckTime(dashTimer) > 3)
             {
-                rigbody.AddForce(-jumpForce * 5, 0, 0);
+                rigbody.AddForce(-jumpForce*dashMulti, 0, 0);
+                dashTimer = Time.time;
+                dashing = true;
             }
             else
             {
@@ -102,7 +111,7 @@ public class AdvancedMovement : MonoBehaviour
             }
         }
     }
-
+    // time difference between 2 times. Confirms user double tapped.
     float CheckTime(float i) {
         return Time.time - i;
     }
