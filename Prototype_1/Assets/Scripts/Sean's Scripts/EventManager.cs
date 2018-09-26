de6c47249b33a10d2c4e6c874168cc31a926c200
Sean Kells <kellssean@myvuw.ac.nz>
2018-09-27 10:54:25 +1200

    New head, revert

---
+++ Prototype_1/Assets/Scripts/Sean's Scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour {
+
+	private Dictionary <string, UnityEvent> eventDictionary;
+
+	private EventManager eventManager;
+
+	public static EventManager instance {
+		get {
+			
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
