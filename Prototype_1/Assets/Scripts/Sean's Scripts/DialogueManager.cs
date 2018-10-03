d0025f23a9848a0f39178ef0adba1f0b7a58683e
Tnx21 <seana.herbert@hotmail.com>
2018-10-03 16:35:00 +1300

    Added dialogue system for displaying text, currently triggered by a test button

---
+++ Prototype_1/Assets/Scripts/Sean's Scripts/DialogueManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue) {
+        animator.SetBool("IsOpen", true);
+
+        Debug.Log("Starting Conversation: ");
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplaynextSentence();
+    }
+
+    public void DisplaynextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue() {
+        Debug.Log("End of conversation");
+        animator.SetBool("IsOpen", false);
+    }
+}
