9d633912d1da22721a946539092266b1d3736801
Sean Kells <kellssean@myvuw.ac.nz>
2018-10-07 18:08:02 +1300

    Fog on, new tree randomiser, need to still do more scene work but getting closer

---
+++ Prototype_1/Assets/Prefabs/Terrain/Trees/LeafRandomiser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafRandomiser : MonoBehaviour {
+
+	Material m_Material;
+
+	Color c;
+
+	// Use this for initialization
+	void Start () {
+		m_Material = GetComponent<Renderer>().material;
+
+		float r = Random.Range(10,40)/255f;
+		float g = Random.Range(65,155)/255f;
+		float b = Random.Range(10,40)/255f;
+
+		c = new Color(r,g,b);
+
+		m_Material.color = c;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
