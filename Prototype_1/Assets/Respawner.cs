f2527c94d7b266e2288bdd583cf174c440bf9626
Guest User <Guest@wg401-f-05-06.mddn.vuw.ac.nz>
2018-10-24 18:40:12 +1300

    Refactored project heirarchy #41, added code for #43, #44, #45, close #43

---
+++ Prototype_1/Assets/Respawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour {
+
+    public GameObject mainSpawn;
+    public GameObject subSpawn;
+
+    public int lives;
+    private int lives_n;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        lives_n = lives;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Mainspawn")){
+            Debug.Log("Updated main spawn");
+            mainSpawn = other.gameObject;
+            subSpawn = other.gameObject;
+        }
+        if (other.CompareTag("Subspawn")){
+            Debug.Log("Updated sub spawn");
+            subSpawn = other.gameObject;
+        }
+        if(other.CompareTag("Killbox")){
+            rb.velocity = new Vector3(0, 0, 0);
+            lives--;
+            Debug.Log("Lives: " + lives);
+            if(lives > 0){
+                transform.position = subSpawn.transform.position;
+            }else{
+                transform.position = mainSpawn.transform.position;
+                lives = lives_n;
+                subSpawn = mainSpawn;
+            }
+        }
+    }
+}
