48fe28498617c8cda8476e5341b56b1d7517a922
Mehau <williams.xrd@gmail.com>
2018-10-24 15:22:42 +1300

    Waseem added Trapdoor trigger and Christian started new menu screen #47

---
+++ Prototype_1/Assets/Scripts/Waseem's Scripts/TrapDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDoor : MonoBehaviour {
+
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.Play("Trapdoor");
+    }
+
+
+}
