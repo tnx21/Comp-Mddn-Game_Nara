185892fb5302f39a5a81377c5616941147db1ac4
Waseem <waseemkoya@gmail.com>
2018-10-30 15:49:20 +1300

    centered camera #49

--- Prototype_1/Assets/Scripts/Waseem's Scripts/PauseMenu.cs
+++ Prototype_1/Assets/Scripts/Waseem's Scripts/PauseMenu.cs
@@ -9,7 +9,6 @@ public class PauseMenu : MonoBehaviour {
 
     public GameObject pauseMenuUI;
 
-	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
