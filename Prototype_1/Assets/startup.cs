b95e28ac9cb74c10c9ac219a3e3fec62230dc208
Tnx21 <seana.herbert@hotmail.com>
2018-10-31 16:55:16 +1300

    can now queue lots of dialogues on the cutscene

---
+++ Prototype_1/Assets/startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startup : MonoBehaviour {
+
+    public Dialogue[] dialogues;
+
+    public void Start()
+    {
+        //FindObjectOfType<DialogueManager>().StartDialogue(dialogue[0]);
+        if (this.dialogues.Length > 0) {
+            FindObjectOfType<DialogueManager>().QueueDialogue(this.dialogues);
+        }
+        
+    }
+}
