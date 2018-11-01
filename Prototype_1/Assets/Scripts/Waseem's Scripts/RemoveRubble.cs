fcb49be155518ca01803cab8f0c4ecd73346f6d3
Waseem Koya <waseemkoya@gmail.com>
2018-11-01 23:29:56 +1300

    created dialogue for altar #63

---
+++ Prototype_1/Assets/Scripts/Waseem's Scripts/RemoveRubble.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveRubble : MonoBehaviour {
+
+    public GameObject rubble;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rubble.SetActive(false);
+    }
+}
---
+++ Prototype_1/Assets/Scripts/Waseem's Scripts/TriggerSecondAltarDialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAlterTrigger : MonoBehaviour {
+
+    public GameObject firstAlterTrigger;
+    public GameObject secondAlterTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        firstAlterTrigger.SetActive(false);
+        secondAlterTrigger.SetActive(true);
+    }
+}
