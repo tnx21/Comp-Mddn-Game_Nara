eab6fb975838bab9b4834ff56db49d6807e44fe4
Tnx21 <seana.herbert@hotmail.com>
2018-11-02 17:27:42 +1300

    fixed bug where trapdoor script would throw error

--- Prototype_1/Assets/Scripts/Waseem's Scripts/TrapDoor.cs
+++ Prototype_1/Assets/Scripts/Waseem's Scripts/TrapDoor.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapDoor : MonoBehaviour {
+public class TrapDoor : MonoBehaviour
+{
 
     public Animator anim;
 
     private void OnTriggerEnter(Collider other)
     {
-        anim.Play("Trapdoor");
+        if (anim != null)
+        {
+            anim.Play("Trapdoor");
+        }
     }
 
 
