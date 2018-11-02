473bf6dca57a8ff8fe53029724d729e0857614e4
Sean Kells <kellssean@myvuw.ac.nz>
2018-11-02 18:14:43 +1300

    Added to bottom right, new assets, bridge added, lift fixed

---
+++ Prototype_1/Assets/LiftBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftBehaviour : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject pointer;
+    public float force, range;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Awake () {
+        rb = player.GetComponent<Rigidbody>();
+        Debug.Log(rb.velocity);
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            float position = (player.transform.position.y - pointer.transform.position.y);
+            if (position < 0) { position = 1; }
+            float scale = 1f - (position / range);
+            Debug.Log(scale + ":" + force);
+            rb.AddForce(0.0f, scale * force, 0.0f);
+        }
+    }
+}
