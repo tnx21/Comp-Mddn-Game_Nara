151c80ed6e2360f798bafb3eb519571d741f1d22
koyawase <waseemkoya@gmail.com>
2018-11-02 20:18:00 +1300

    finalised level design #19

---
+++ Prototype_1/Assets/Scripts/Waseem's Scripts/AttachPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachPlayer : MonoBehaviour
+{
+
+    public GameObject Player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Debug.Log("ENTERED");
+            Player.transform.parent = this.transform.parent;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
