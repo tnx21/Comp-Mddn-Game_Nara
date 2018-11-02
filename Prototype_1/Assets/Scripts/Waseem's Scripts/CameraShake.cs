16f9f90b82868b1310413c94bd5077e16f035c39
koyawase <waseemkoya@gmail.com>
2018-11-02 14:16:18 +1300

    implemented camera shake to replicate earthquake #64

---
+++ Prototype_1/Assets/Scripts/Waseem's Scripts/CameraShake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera;
+    bool shaking = false;
+
+    public void Update()
+    {
+        if (shaking)
+        {
+            Shake();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        shaking = true;
+    }
+
+    public void Shake()
+    {
+        Kino.AnalogGlitch glitch = this.playerCamera.GetComponent<Kino.AnalogGlitch>();
+        glitch.enabled = true;
+        StartCoroutine(disableGlitch(glitch));
+    }
+
+    IEnumerator disableGlitch(Kino.AnalogGlitch glitch)
+    {
+        yield return new WaitForSeconds(2.0f);
+        glitch.enabled = false;
+        shaking = false;
+    }
+
+
+}
